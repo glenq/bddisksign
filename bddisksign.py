c759aa5fafb1726abe0c61ace666da6787cf9eb8
Glen <glenqu@hotmail.com>
2023-04-28 00:33:11 +0800

    Create bddisksign.py

---
+++ bddisksign.py
@@ -0,0 +1,95 @@
+# -*- coding:utf-8 -*-
+# @Time : 2022/4/11 10:30
+# @Author: qcodeq
+# @File : 网盘签到.py
+import requests
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import socket  # 用于端口检测
+import base64  # 用于编解码
+import json  # 用于Json解析
+import os  # 用于导入系统变量
+import sys  # 实现 sys.exit
+import logging  # 用于日志输出
+import time  # 时间
+import re  # 正则过滤
+import hmac
+import struct
+
+
+def check_in(cookie):
+    headers = {
+        'User-Agent': 'netdisk;11.19.11;M2012K11AC;android-android;11;JSbridge4.4.0;jointBridge;1.1.0',
+        'Referer': 'https://pan.baidu.com/act/task/mainpage',
+        'Cookie': cookie,
+    }
+
+    sign_url = 'https://pan.baidu.com/pmall/points/signin'
+    check_url = 'https://pan.baidu.com/pmall/points/balance'
+    param = {
+        'channel': 'android_11_M2012K11AC_bd-netdisk_1024328u',
+        'time': int(time.time())
+    }
+
+    response_json = requests.get(url=sign_url, params=param, headers=headers).json()
+    check_json = requests.get(url=check_url, params=param, headers=headers).json()
+    balance = check_json['balance']
+    if response_json['errno'] == 0:
+        points = response_json['points']
+        dic = {
+            'points': points,
+            'balance': balance
+        }
+        return dic
+    else:
+        pass
+    print('休息三秒，执行下一个账号'.center(50, '='))
+    time.sleep(3)
+
+    return "error"
+
+# return list[bd_ck]
+def get_bdck():  # 方法 获取 bd_ck值 [系统变量传递]
+    if "bd_ck" in os.environ:  # 判断 bd_ck是否存在于环境变量
+        bd_ck_list = os.environ['bd_ck'].split('&')  # 读取系统变量 以 & 分割变量
+        if len(bd_ck_list) > 0:  # 判断 bd_ck 数量 大于 0 个
+            return bd_ck_list  # 返回 bd_ck [LIST]
+        else:  # 判断分支
+            logger.info("bd_ck变量未启用")  # 标准日志输出
+            sys.exit(1)  # 脚本退出
+    else:  # 判断分支
+        logger.info("未添加bd_ck变量")  # 标准日志输出
+        sys.exit(0)  # 脚本退出
+
+
+# 任务完成发送邮件函数
+def send_email(rs):
+    # 登录
+    smtp_obj = smtplib.SMTP_SSL('smtp.qq.com', 465)
+    smtp_obj.login('', '')
+
+    # 邮件内容
+    str_success = ''
+    str_fail = "sign fail, check script"
+    for info in rs:
+        get_now = info['points']
+        balance = info['balance']
+        str_success = str_success + f'get {get_now}积分，目前总积分{balance}\n'
+
+    if len(rs) != 0:
+        msg = MIMEText(str_success, 'plain', 'utf-8')
+        msg['Subject'] = Header("bd disk sign success", 'utf-8')
+    else:
+        msg = MIMEText(str_fail, 'palin', 'utf-8')
+        msg['Subject'] = Header("bd disk sign fail", 'utf-8')
+    smtp_obj.sendmail("", [""], msg.as_string())
+
+
+if __name__ == '__main__':
+    bdcklist = get_bdck()
+    for ck in bdcklist:
+        rs = check_in(ck)
+        print(rs)
+#       send_email(rs)
